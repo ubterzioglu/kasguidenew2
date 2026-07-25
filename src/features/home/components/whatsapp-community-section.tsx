e590276554ae610ee22ccb1c81b9de7683d11a47
Umut Baris Terzioglu <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-07-25 21:27:37 +0200

    add partner links to footer

--- src/features/home/components/whatsapp-community-section.tsx
+++ src/features/home/components/whatsapp-community-section.tsx
@@ -134,15 +134,36 @@ export function WhatsAppCommunitySection() {
               {' | '}
               <a href="https://tekhurdametal.com/hurda-fiyatlari/" rel="dofollow">
                 Hurda Fiyatları
-              </a>
+              </a>{' '}
+              Tek Hurda Metal
               {' | '}
               <a href="https://tekhurdametal.com/istanbul-hurdaci/" rel="dofollow">
                 İstanbul Hurdacı
               </a>{' '}
               Firması Tek Hurda Metal A.Ş
               {' | '}
+              <a href="https://tekhurdametal.com/hurda-demir-fiyatlari/" rel="dofollow">
+                Demir Hurda Fiyatları
+              </a>{' '}
+              Tek Hurda Metal
+              {' | '}
+              <a href="https://tekhurdametal.com/beylikduzu-hurdaci/" rel="dofollow">
+                Beylikdüzü Hurdacı
+              </a>{' '}
+              Firması Tek Hurda Metal A.Ş
+              {' | '}
               <a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">
                 Antalya Sex Shop
+              </a>{' '}
+              Lion Erotik
+              {' | '}
+              <a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">
+                Antalya Erotik Shop
+              </a>{' '}
+              Lion Erotik
+              {' | '}
+              <a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">
+                Antalya Seks Shop
               </a>{' '}
               Lion Erotik
               {' | '}Bir başkadır Kaş!
